340aa5d415596fcc04c21105b6304b4d33a108ed
Maksim <57707242+maksim-nemtsev@users.noreply.github.com>
2020-03-24 22:16:50 +0300

    added a third challenge

---
+++ base-apparel-coming-soon-master/script.js
@@ -0,0 +1,20 @@
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const emailVal = email.value;
+
+  if (!validateEmail(emailVal)) {
+    form.classList.add('error');
+    form.classList.remove("success");
+  } else {
+    form.classList.remove("error");
+    form.classList.add("success");
+  }
+});
+
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
