89fa23984ca522ee405e8e2fbe87edcbd9f18ce7
Maksim <maksim-nemtsev@users.noreply.github.com>
2020-04-05 22:27:57 +0300

    add commits

--- base-apparel-coming-soon-master/script.js
+++ base-apparel-coming-soon-master/script.js
@@ -14,6 +14,8 @@ form.addEventListener('submit', e => {
   }
 });
 
+// validate email
+
 function validateEmail(email) {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
